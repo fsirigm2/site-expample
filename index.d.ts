84243957993df2a3a95f2e3d9e8c43b22ed307d1
fsirigm2 <fernando.siri@gm2dev.com>
2024-12-04 12:48:01 -0300

    removed types usage

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@fsirigm2/obe-example" {
-  import { HeroSearchFormProps } from "./components/central-picker/HeroSearchForm";
-
-  export const HeroSearchForm: React.FC<HeroSearchFormProps>;
-  export const tailwindConfig: any;
-}
