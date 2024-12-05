cc3317a0b4b3832373bebbdfd1bd725b0c88b8e0
fsirigm2 <fernando.siri@gm2dev.com>
2024-12-05 13:35:13 -0300

    import several components

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 
 "use client";
-import { HeroSearchForm } from "@fsirigm2/obe-example";
+import HeroSearchForm from "@fsirigm2/obe-example/HeroSearch";
+import HeroSearchForm2 from "@fsirigm2/obe-example/HeroSearch2";
+import HeroSearchForm3 from "@fsirigm2/obe-example/HeroSearch3";
+import HeroSearchForm4 from "@fsirigm2/obe-example/HeroSearch4";
+
+
 
 export default function Home() {
   return (
     <div
       className="w-full h-screen"
     >
-      <HeroSearchForm className=""/>
+      <HeroSearchForm className="m-auto"/>
+      <HeroSearchForm2 className="m-auto"/>
+      <HeroSearchForm3 className="m-auto"/>
+      <HeroSearchForm4 className="m-auto"/>
+      
     </div>
   );
 }
